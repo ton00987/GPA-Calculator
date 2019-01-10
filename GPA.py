019ebd31493d853dc5ac0b4e81bf63f20128b502
ton00987 <theton0087@hotmail.com>
2019-01-10 20:49:18 +0700

    Can edit grade

--- GPA.py
+++ GPA.py
@@ -24,6 +24,32 @@ def insert_grade():
     writer.writerows(reader)
   f.close()
 
+def edit_grade():
+  change_subject = input('Enter subject code: ')
+  edit_semester = input('Enter semester: ')
+  edit_subject = input('Enter subject: ')
+  edit_credit = input('Enter credit: ')
+  edit_grade = input('Enter grade: ')
+  edit_row = [edit_semester, edit_subject, edit_credit, edit_grade]
+
+  with open('Calculate GPA.csv', 'r') as f:
+    reader = csv.reader(f)
+    reader = list(reader)
+    reader.pop(0)
+    for i in range(len(reader)):
+      if reader[i][1][:9] == change_subject:
+        reader[i] = edit_row
+
+    reader.sort()
+  f.close()
+
+  header = ['Semester', 'Subject', 'Credits', 'Grade']
+  with open('Calculate GPA.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(reader)
+  f.close()
+
 if command == '1':
   insert_grade()
 elif command == '2':
