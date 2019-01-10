e8232d7694db0bf56815b8f20f48f0777df3c55b
ton00987 <theton0087@hotmail.com>
2019-01-10 20:13:04 +0700

    Add subject code to csv and can insert new subject

--- GPA.py
+++ GPA.py
@@ -1,9 +1,30 @@
 import csv
 
-all_grade = {}
+command = input('Insert: 1 Edit: 2\n')
 
-with open('Calculate GPA.csv', 'r') as f:
-  reader = csv.reader(f)
-  next(reader)
-  for row in reader:
-    print(row)
+def insert_grade():
+  new_semester = input('Enter Semester: ')
+  new_subject = input('Enter subject: ')
+  new_credit = input('Enter credit: ')
+  new_grade = input('Enter grade: ')
+  new_row = [new_semester, new_subject, new_credit, new_grade]
+
+  with open('Calculate GPA.csv', 'r') as f:
+    reader = csv.reader(f)
+    reader = list(reader)
+    reader.pop(0)
+    reader.append(new_row)
+    reader.sort()
+  f.close()
+
+  header = ['Semester', 'Subject', 'Credits', 'Grade']
+  with open('Calculate GPA.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(reader)
+  f.close()
+
+if command == '1':
+  insert_grade()
+elif command == '2':
+  edit_grade()
