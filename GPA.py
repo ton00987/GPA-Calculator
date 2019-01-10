79eda3821a4d9892b0c8a8c0202859a0cebe9d94
ton00987 <theton0087@hotmail.com>
2019-01-10 21:51:55 +0700

    Can show grade and show grade after insert or edit

--- GPA.py
+++ GPA.py
@@ -1,6 +1,7 @@
 import csv
+import math
 
-command = input('Insert: 1 Edit: 2\n')
+command = input('Insert: 1 Edit: 2 Show: 3\n')
 
 def insert_grade():
   new_semester = input('Enter Semester: ')
@@ -23,6 +24,8 @@ def insert_grade():
     writer.writerow(header)
     writer.writerows(reader)
   f.close()
+
+  show_grade()
 
 def edit_grade():
   change_subject = input('Enter subject code: ')
@@ -50,7 +53,37 @@ def edit_grade():
     writer.writerows(reader)
   f.close()
 
+  show_grade()
+
+def show_grade():
+  with open('Calculate GPA.csv', 'r') as f:
+    reader = csv.reader(f)
+    reader = list(reader)
+    reader.pop(0)
+
+    print('{:<10}{:^60}{:^11}{:^9}'.format('Semester', 'Subject', 'Credits', 'Grade'))
+
+    before_semester = '1'
+    sum_product = 0
+    sum_credit = 0
+
+    for i in range(len(reader)):
+      if reader[i][0] != before_semester:
+        print('{:^8}       {:<55}'.format('GPA', str(math.floor(sum_product/sum_credit*100)/100)))
+        sum_product = 0
+        sum_credit = 0
+      print('{:^8}       {:<55}{:^11}{:^9}'.format(reader[i][0], reader[i][1], reader[i][2], reader[i][3]))
+      sum_product += float(reader[i][2]) * float(reader[i][3])
+      sum_credit += float(reader[i][2])
+
+      before_semester = reader[i][0]
+
+    print('{:^8}       {:<55}'.format('GPA', str(math.floor(sum_product/sum_credit*100)/100)))
+  f.close()
+
 if command == '1':
   insert_grade()
 elif command == '2':
   edit_grade()
+elif command == '3':
+  show_grade()
